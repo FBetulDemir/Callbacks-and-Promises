5060adbfedd995dee36c821415d8225be08dafdb
FBetulDemir <fatmabetuld@gmail.com>
2024-10-02 11:26:01 +0200

    Added  JavaScript rollDice function that returns a promise.

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,13 @@
+//  JavaScript function that returns a promise.
 const rollDice = () => {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    if (randomDice === 6) {
+      resolve("You rolled a 6! You win!");
+    } else {
+      reject(
+        `You rolled a ${randomDice} and you lost. Try your chance another time!`
+      );
+    }
+  });
 };
